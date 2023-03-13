596195c960a1a4e3c96487698a0a12a949c464d3
CalvinY8 <46547265+CalvinY8@users.noreply.github.com>
2023-03-12 18:09:01 -0700

    Update heart_printer.cpp

--- heart_printer.cpp
+++ heart_printer.cpp
@@ -3,49 +3,55 @@
 #include <algorithm>
 #include <vector>
 
-void print_list_alphabetical(std::vector<std::string> str_vect, std::string title){
-    
-    //sort alphabetically
-    //ascending order by default
-    std::sort(str_vect.begin(), str_vect.end());
-    
-    std::cout << title << std::endl;
-    
-    for (std::string x : str_vect){
-    	std::cout << x << std::endl;
-    }
-    
-}
+class printer {
 
-//print a heart with slashes
-//minimal hard-coding
-void print_heart(){
-    std::string str_a = "/ \\";
-    
-    std::cout << str_a + str_a << std::endl;
-    
-    for (int i = 0; i < 3; i++){
+    public:
 
-        /*
-        outer_spaces \ middle_spaces / outer_spaces
-        */
+        void print_list_alphabetical(std::vector<std::string> str_vect, std::string title){
+            
+            //sort alphabetically
+            //ascending order by default
+            std::sort(str_vect.begin(), str_vect.end());
+            
+            std::cout << title << std::endl;
+            
+            for (std::string x : str_vect){
+                std::cout << x << std::endl;
+            }
+            
+        }
 
-        std::string outer_spaces = std::string(i, ' ');
-        std::string middle_spaces = std::string((2 - i) * 2, ' ');
-        
-        std::cout << outer_spaces + "\\" + middle_spaces + str_a[0] + outer_spaces << std::endl;
+        //print a heart with slashes
+        //minimal hard-coding
+        void print_heart(){
+            std::string str_a = "/ \\";
+            
+            std::cout << str_a + str_a << std::endl;
+            
+            for (int i = 0; i < 3; i++){
 
-    }
-    
-    /*
-    
-    / \/ \
-    \    /    (0 spaces)\(4 spaces)/(0spaces)
-     \  /     (1 spaces)\(2 spaces)/(1 spaces)
-      \/      (2 spaces)\(0 spaces)/(2 spaces)
-    
-    */
-}
+                /*
+                outer_spaces \ middle_spaces / outer_spaces
+                */
+
+                std::string outer_spaces = std::string(i, ' ');
+                std::string middle_spaces = std::string((2 - i) * 2, ' ');
+                
+                std::cout << outer_spaces + "\\" + middle_spaces + str_a[0] + outer_spaces << std::endl;
+
+            }
+            
+            /*
+
+            / \/ \
+            \    /    (0 spaces)\(4 spaces)/(0spaces)
+             \  /     (1 spaces)\(2 spaces)/(1 spaces)
+              \/      (2 spaces)\(0 spaces)/(2 spaces)
+
+            */
+        }
+
+};
 
 int main() {
 
@@ -62,11 +68,13 @@ int main() {
     "jaffa cake",
     "shortbread",
     "biscoff",
-    "chocolate digestive" 
+    "chocolate digestive"
     };
 
-    print_list_alphabetical(cookie_vect, title);
-    print_heart();
+    printer printer_inst;
+
+    printer_inst.print_list_alphabetical(cookie_vect, title);
+    printer_inst.print_heart();
 
     return 0;
 }
